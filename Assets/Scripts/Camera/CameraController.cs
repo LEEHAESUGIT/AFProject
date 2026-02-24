b08c5b55f21f3d52587d1f8aa46d400a7cdecba2
LEEHAESUGIT <child6493@gmail.com>
2026-02-24 21:07:21 +0900

    Initial commit

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    Camera mainCamera;
+
+    
+    void Start()
+    {
+        mainCamera = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+    }
+}
